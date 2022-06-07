5ba85e148fa6a38781d1a7d7035e91e8da128947
Gurminder Kaler <gurminderkaler@Gurminders-MacBook-Air.local>
2022-06-07 13:57:01 -0400

    sd

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8df07a27345cdd85"; }
+/******/ 		__webpack_require__.h = function() { return "2e934a24e72e02c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ client/pages/frontend/about/index.js
@@ -0,0 +1,12 @@
+import aboutCss from '@/styles/about.module.css';
+function about() { 
+    return (
+        <>
+            <h2 className={aboutCss.abc}>About Us page</h2>
+        </>
+    )
+}
+
+export default about
+
+
